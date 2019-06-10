b9a62f8a08e0ac7f9e89f1f30ce4f6d9a6c52a26
MartinLenglet <martinlenglet@gmail.com>
2019-06-10 18:24:21 +0200

    tout

---
+++ Democrazik/src/main/java/fr/democrazikDao/TestRepository.java
@@ -0,0 +1,9 @@
+package fr.democrazikDao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import fr.democrazik.entities.Test;
+
+public interface TestRepository extends JpaRepository<Test, Long> {
+
+}
