ea5123cc1ebd554acfc5d06989bfb28944b2d495
ESIC <ESIC@192.168.1.32>
2019-06-10 12:05:18 +0200

    Création projet

---
+++ Democrazik/src/main/java/fr/solutec/democrazik/DemocrazikApplication.java
@@ -0,0 +1,13 @@
+package fr.solutec.democrazik;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemocrazikApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemocrazikApplication.class, args);
+	}
+
+}
