58e86e48c64b6863b9d282f44cb7bee05d3fa8b7
Nathael <natecgg@sfr.fr>
2019-06-11 21:10:01 +0200

    nouvelle methode dans morceaurepository

--- Democrazik/src/main/java/fr/democrazik/dao/MorceauRepository.java
+++ Democrazik/src/main/java/fr/democrazik/dao/MorceauRepository.java
@@ -1,9 +1,22 @@
 package fr.democrazik.dao;
 
+import java.util.List;
+
+import javax.persistence.EntityManager;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import fr.democrazik.entities.Morceau;
+import fr.democrazik.entities.Session;
 
 public interface MorceauRepository extends JpaRepository<Morceau, Long> {
+	
+
+	@Query("select morceau, compte from Morceau morceau, "
+			+ "(select vote, count(vote) as compte from Vote vote groub by vote.idTitre) table"
+			+ "where morceau.idTitre = table.vote.idTitre ORDER BY table.compte")
+	List<Object[]> values();
+	
 
 }
