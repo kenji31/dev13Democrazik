161d4adb39df870f3ff8cd99c638dc053c63d43f
kenji31 <50909586+kenji31@users.noreply.github.com>
2019-06-13 09:45:24 +0200

    Add files via upload

---
+++ Democrazik/src/main/java/fr/democrazik/dao/SessionRepository.java
@@ -0,0 +1,17 @@
+package fr.democrazik.dao;
+
+import java.util.List;
+
+import javax.transaction.Transactional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+
+
+import fr.democrazik.entities.Session;
+
+public interface SessionRepository extends JpaRepository<Session, Long> {
+
+	public List<Session> findSessionByNom(String nom);
+
+}
