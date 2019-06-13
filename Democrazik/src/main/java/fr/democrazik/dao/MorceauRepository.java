d6d812989f73ca6c6ab6604804b10eb92a425f32
Nathael <natecgg@sfr.fr>
2019-06-13 18:10:02 +0200

    ton code bye bye

--- Democrazik/src/main/java/fr/democrazik/dao/MorceauRepository.java
+++ Democrazik/src/main/java/fr/democrazik/dao/MorceauRepository.java
@@ -7,7 +7,6 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
-import fr.democrazik.entities.Collection;
 import fr.democrazik.entities.Morceau;
 
 
--- Democrazik/src/main/java/fr/democrazik/web/MorceauRest.java
+++ Democrazik/src/main/java/fr/democrazik/web/MorceauRest.java
@@ -12,7 +12,6 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import fr.democrazik.dao.MorceauRepository;
-import fr.democrazik.entities.Collection;
 import fr.democrazik.entities.Morceau;
 import fr.democrazik.entities.Vote;
 
