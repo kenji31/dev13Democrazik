b532d8feb6d9138aba288c1a629a1ca89a942db2
Natecgg <50909629+Natecgg@users.noreply.github.com>
2019-06-16 19:06:53 +0200

    Update ArtisteRepository.java

--- Democrazik/src/main/java/fr/democrazik/dao/ArtisteRepository.java
+++ Democrazik/src/main/java/fr/democrazik/dao/ArtisteRepository.java
@@ -14,7 +14,7 @@ public interface ArtisteRepository extends JpaRepository<Artiste, Long>{
 
 	public List<Artiste> findArtisteByNom(String nom);
 	
-	@Query("selesct id from artiste left join morceau on artiste.id = morceau.artiste_id where artiste_id IS NULL", 
+	@Query("selesct artiste.id from artiste left join morceau on artiste.id = morceau.artiste_id where artiste_id IS NULL", 
 	       native = true)
 	public List<Long> idOfUselessArtists();
 }
