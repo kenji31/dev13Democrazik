1784b53fc22605a371f0d5334611c7026abdfc11
Natecgg <50909629+Natecgg@users.noreply.github.com>
2019-06-16 16:52:19 +0200

    Update MorceauRepository.java
    !! id_session ou session_id ? voir la base sql générée

--- Democrazik/src/main/java/fr/democrazik/dao/MorceauRepository.java
+++ Democrazik/src/main/java/fr/democrazik/dao/MorceauRepository.java
@@ -13,7 +13,10 @@ import fr.democrazik.entities.Morceau;
 public interface MorceauRepository extends JpaRepository<Morceau, Long> {
 	
 	@Query("select v, count(v) as compte from Vote v group by v.morceau.id order by compte desc")
-	public List<Object[]> orderMorceauByVote();	
+	public List<Object[]> orderMorceauByVote();
+	
+	@Query(value ="select chemin from Morceau where id_session=?1 ORDER BY RAND() LIMIT 1", native=true) // id_session ou session_id ?
+	public String getVotesOfMorceau(Long id);
 	
 }
  
