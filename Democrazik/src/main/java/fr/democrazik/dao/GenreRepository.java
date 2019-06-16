592a8f74632c2d6c7384056d23431638501bb940
Natecgg <50909629+Natecgg@users.noreply.github.com>
2019-06-16 19:06:39 +0200

    Update GenreRepository.java

--- Democrazik/src/main/java/fr/democrazik/dao/GenreRepository.java
+++ Democrazik/src/main/java/fr/democrazik/dao/GenreRepository.java
@@ -8,4 +8,8 @@ import fr.democrazik.entities.Genre;
 
 public interface GenreRepository extends JpaRepository<Genre, Long>{
 	public List<Genre> findGenreByLibelle(String nom);
+	
+	@Query("selesct genre.id from genre left join morceau on genre.id = morceau.genre_id where genre_id IS NULL", 
+	       native = true)
+	public List<Long> idOfUselessGenres();
 }
